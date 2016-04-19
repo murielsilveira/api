3255873faac3bf760ed8b0d01a4d3005d29ec2fd
Muriel Silveira <murielkong@gmail.com>
2016-04-18 22:26:27 -0300

    Add loggging

--- server.js
+++ server.js
@@ -1,24 +1,41 @@
 'use strict'
 
 const Hapi = require('hapi')
-
 const server = new Hapi.Server()
+
 server.connection({
-	host: 'localhost',
-	port: 5000
+    host: 'localhost',
+    port: 5000
 })
 
 server.route({
-	method: 'GET',
-	path: '/',
-	handler: function (request, reply) {
-		return reply('hello hapi')
-	}
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        return reply('hello hapi')
+    }
 })
 
-server.start((err) => {
-	if (err)
-		throw err
+server.register({
+    register: require('good'),
+    options: {
+        reporters: [{
+            reporter: require('good-console'),
+            events: {
+                log: '*',
+                error: '*'
+            }
+        }]
+    }
+}, function(err) {
+    if (err)
+        throw err
 
-	console.log('Server running at:', server.info.uri)
+    server.start((err) => {
+        if (err)
+            throw err
+
+        console.log('Server running at:', server.info.uri)
+    })
 })
+
